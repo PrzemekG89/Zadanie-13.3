bb9f9e07d0467e582c27d6be971795dd35ffddaa
Przemek Gałdys <galdys.przemek@gmail.com>
2017-11-29 17:09:21 +0100

    Node.js - first time

---
+++ programs.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case 'exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break
+            case 'version':
+                process.stdout.write(process.version + '\n');
+                break
+            case 'system':
+                process.stdout.write(process.env + '\n');
+                break
+            default:
+                process.stderr.write('Wrong instruction!\n');
+                break
+        };
+    }
+});
